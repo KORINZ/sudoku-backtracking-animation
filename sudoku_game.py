23a66c54d9e321b6943f888c15afdff9e5ae2b0d
KORINZ <111611023+KORINZ@users.noreply.github.com>
2023-03-13 23:20:50 +0900

    Add comments for menu class

--- sudoku_game.py
+++ sudoku_game.py
@@ -50,11 +50,14 @@ class Menu():
     def make_centered_button(self, name: str, starting_y: int) -> Tuple[int, int, int, int]:
         button = self.font.render(name, True, self.color)
         WIN.blit(button, (WIDTH / 2 - button.get_width() // 2, starting_y))
+
+        # Starting x and y points of the button
         button_starting_x, button_starting_y = (
             WIDTH - button.get_width()) // 2, starting_y
         pygame.draw.rect(WIN, BLACK, (button_starting_x, button_starting_y, button.get_width(
         ), button.get_height()), width=self.line_width)
 
+        # Ending x and y points of the button
         button_ending_x, button_ending_y = button_starting_x + \
             button.get_width(), button_starting_y + button.get_height()
         return button_starting_x, button_starting_y, button_ending_x, button_ending_y
