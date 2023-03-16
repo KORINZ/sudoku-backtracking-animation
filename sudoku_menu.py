a044d8cab3c3a8eb1145a90e4f822da6e1eac31f
KORINZ <111611023+KORINZ@users.noreply.github.com>
2023-03-16 22:32:36 +0900

    Fix black screen with pygame.display.get_surface()

--- sudoku_menu.py
+++ sudoku_menu.py
@@ -1,14 +1,12 @@
 import pygame
 from typing import Tuple
 
-WIDTH, HEIGHT = 720, 720
-
 
 class Menu():
     def __init__(self) -> None:
-        self.WIDTH = 720
-        self.HEIGHT = 720
-        self.WIN = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+        self.WIN = pygame.display.get_surface()
+        self.WIDTH = pygame.display.get_surface().get_size()[0]
+        self.HEIGHT = pygame.display.get_surface().get_size()[1]
         self.color = (0, 0, 0)
         self.font = pygame.font.Font('ipaexg.ttf', 40)
         self.line_width = 3
@@ -17,11 +15,11 @@ class Menu():
     def make_centered_button(self, name: str, starting_y: int) -> Tuple[int, int, int, int]:
         button = self.font.render(name, True, self.color)
         self.WIN.blit(
-            button, (WIDTH / 2 - button.get_width() // 2, starting_y))
+            button, (self.WIDTH / 2 - button.get_width() // 2, starting_y))
 
         # Starting x and y points of the button
         button_starting_x, button_starting_y = (
-            WIDTH - button.get_width()) // 2, starting_y
+            self.WIDTH - button.get_width()) // 2, starting_y
         pygame.draw.rect(self.WIN, self.color, (button_starting_x, button_starting_y, button.get_width(
         ), button.get_height()), width=self.line_width)
 
